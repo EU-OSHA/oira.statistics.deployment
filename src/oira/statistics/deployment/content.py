8b5e257fb6c419e07ac0600fa678e526adcf6944
ale-rt <alessandro.pisa@gmail.com>
2021-04-27 11:58:13 +0200

    Added a new card Number Of Users And Assessments By Tools in the tools dashboard

--- src/oira/statistics/deployment/content.py
+++ src/oira/statistics/deployment/content.py
@@ -399,6 +399,88 @@ class ToolsCardFactory(CardFactory):
                     "graph.dimensions": ["zodb_path"],
                     "graph.metrics": ["Users per Year"],
                 },
+                "height": 8,
+                "width": 8,
+            },
+            "number_of_users_and_assessments_by_tools": {
+                "name": "Number Of Users And Assessments By Tools",
+                "display": "table",
+                "query_type": "query",
+                "dataset_query": {
+                    "type": "query",
+                    "query": {
+                        "source-table": self.table_id,
+                        "expressions": {
+                            "Users per Year": [
+                                "/",
+                                ["field-id", self.fields["num_users"]],
+                                ["+", ["field-id", self.fields["years_online"]], 1],
+                            ]
+                        },
+                        "fields": [
+                            ["field-id", self.fields["zodb_path"]],
+                            ["field-id", self.fields["num_users"]],
+                            ["field-id", self.fields["num_assessments"]],
+                        ],
+                        "order-by": [
+                            ["desc", ["field-id", self.fields["num_assessments"]]]
+                        ],
+                    },
+                    "database": self.database_id,
+                },
+                "result_metadata": [
+                    {
+                        "base_type": "type/Text",
+                        "display_name": "Zo Db Path",
+                        "name": "zodb_path",
+                        "special_type": "type/PK",
+                        "fingerprint": None,
+                    },
+                    {
+                        "base_type": "type/Integer",
+                        "display_name": "Num Users",
+                        "name": "num_users",
+                        "special_type": "type/Quantity",
+                        "fingerprint": {
+                            "global": {"distinct-count": 8, "nil%": 0.0},
+                            "type": {
+                                "type/Number": {
+                                    "min": 1.0,
+                                    "q1": 1.3542486889354093,
+                                    "q3": 3.803847577293368,
+                                    "max": 20.0,
+                                    "sd": 3.632547632014008,
+                                    "avg": 3.3333333333333335,
+                                }
+                            },
+                        },
+                    },
+                    {
+                        "base_type": "type/Integer",
+                        "display_name": "Num Assessments",
+                        "name": "num_assessments",
+                        "special_type": "type/Quantity",
+                        "fingerprint": {
+                            "global": {"distinct-count": 14, "nil%": 0.0},
+                            "type": {
+                                "type/Number": {
+                                    "min": 1.0,
+                                    "q1": 1.5,
+                                    "q3": 9.0,
+                                    "max": 39.0,
+                                    "sd": 7.984467680311008,
+                                    "avg": 6.8,
+                                }
+                            },
+                        },
+                    },
+                ],
+                "visualization_settings": {
+                    "table.pivot": False,
+                    "table.pivot_column": "num_users",
+                    "table.cell_column": "num_assessments",
+                },
+                "height": 8,
                 "width": 8,
             },
         }
